9961ca972683c9e18595d1c44c288914b8402296
iliangood <ilia191168@gmail.com>
2025-10-16 19:07:37 +0300

    Перенес axis в отдельные файлы

--- submarine.ino
+++ submarine.ino
@@ -2,70 +2,8 @@
 #include <message.h>
 #include <utils.h>
 
-enum class AxisesNames
-{
-    Vx = 0,
-    Vy = 1,
-    Vz = 2,
-    Wx = 3,
-    Wy = 4,
-    Wz = 5
-};
+#include "axis.h"
 
-class Axises
-{
-    int16_t axises[6];
-public:
-    Axises() : axises{0, 0, 0, 0, 0, 0} {}
-
-    Axises(const int16_t* axises)
-    {
-        for (char i = 0; i < 6; ++i)
-        {
-            this->axises[i] = clamp(axises[i], -256, 256);
-        }
-    }
-
-    int16_t& operator[](int index)
-    {
-        return axises[index];
-    }
-
-    int16_t& operator[](AxisesNames index)
-    {
-        return axises[static_cast<int>(index)];
-    }
-
-    int16_t operator[](AxisesNames index) const
-    {
-        return axises[static_cast<int>(index)];
-    }
-
-    int16_t operator[](int index) const
-    {
-        return axises[index];
-    }
-
-    Axises(int16_t Vx, int16_t Vy, int16_t Vz, int16_t Wx, int16_t Wy, int16_t Wz)
-    {
-        (*this)[AxisesNames::Vx] = clamp(Vx, -256, 256);
-        (*this)[AxisesNames::Vy] = clamp(Vy, -256, 256);
-        (*this)[AxisesNames::Vz] = clamp(Vz, -256, 256);
-        (*this)[AxisesNames::Wx] = clamp(Wx, -256, 256);
-        (*this)[AxisesNames::Wy] = clamp(Wy, -256, 256);
-        (*this)[AxisesNames::Wz] = clamp(Wz, -256, 256);
-    }
-
-    int getAxis(AxisesNames axis) const
-    {
-        return axises[static_cast<int>(axis)];
-    }
-
-    int getAxis(int axis) const
-    {
-        return axises[axis];
-    }
-};
 
 class Motor
 {
