365006d36b658f052d2e4f6a8fa4c888dae92b57
iliangood <ilia191168@gmail.com>
2026-01-07 22:27:45 +0300

    Работа с акселерометром

--- sensors.h
+++ sensors.h
@@ -1,7 +1,19 @@
 #if defined !(SENSORS_H)
 #define SENSORS_H
+#include <MPU6050_light.h>
+#include <Wire.h>
 class accelerometer
 {
+  MPU6050 mpu;
 public:
+  accelerometer(TwoWire wire) : mpu(wire) {}
+  uint8_t init() {
+    Serial.println("Калибровка акселерометра держите датчик неподвижно горизонтально!");
+    uint8_t rc = mpu.begin();
+    mpu.calcOffsets();
+  }
+  Axises getMove() {
+    mpu.
+  }
 };
 #endif
