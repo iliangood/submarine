8ac65c08623b01a7f6ad7ed3fb87e0b7490f3959
iliangood <ilia191168@gmail.com>
2026-02-08 22:36:40 +0300

    Работаю над стабилизацией

---
+++ PID.h
@@ -0,0 +1,16 @@
+#if !defined PID_H
+#define PID_H
+
+class PID
+{
+  int16_t p_;
+  int16_t i_;
+  int16_t d_;
+  int16_t current_;
+  int16_t prev_;
+  int16_t target_;
+public:
+  PID( int16_t p, int16_t i, int16_t d) : p_(p), i_(i), d_(d), current_(0), prev_(0), target_(0) {}
+};
+
+#endif
