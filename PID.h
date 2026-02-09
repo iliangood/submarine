f44f4933f69deedc10d6a9ea9b20031ecbd3fda9
iliangood <ilia191168@gmail.com>
2026-02-09 22:06:12 +0300

    Работа над стабилищацией

--- PID.h
+++ PID.h
@@ -11,6 +11,7 @@ class PID
   int16_t target_;
 public:
   PID( int16_t p, int16_t i, int16_t d) : p_(p), i_(i), d_(d), current_(0), prev_(0), target_(0) {}
+  setTarget(int16_t target);
 };
 
 #endif
