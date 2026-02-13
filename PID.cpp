09a7e83bf4b05ca96084ce8d2a24c034ba0f743a
iliangood <ilia191168@gmail.com>
2026-02-13 22:36:38 +0300

    Работа над PID

--- PID.cpp
+++ PID.cpp
@@ -8,12 +8,17 @@ void PID::setTarget(int16_t target)
 
 int16_t PID::update(int16_t current)
 {
-  uint16_t time_collapsed = constrain(millis() - last_update, 0, UINT16_MAX);
+  uint16_t time_elapsed = constrain(millis() - last_update, 1, 32);
+
   int32_t diff = static_cast<in32_t>(target) - static_cast<in32_t>(current);
-  int32_t prev_diff = static_cast<in32_t>(target) - static_cast<in32_t>(prev_);
-  int16_t p_component = (diff * p_)/INT16_MAX;
-  i_component = constrain(i_component + (diff * i_)/INT16_MAX, INT16_MIN, INT16_MAX);
-  int16_t d_component = (prev_diff - diff)/diff*d_ 
-  prev_ = current_;
-  return p_component + i_component + d_component;
+  
+  int16_t p_component = (static_cast<int32_t>(diff) * p_)/INT16_MAX;
+
+  i_component = constrain(i_component + constrain((static_cast<int64_t>(diff) * i_ * time_elapsed)/INT16_MAX, -1024, 1023), INT16_MIN, INT16_MAX);
+  
+  int16_t change_rate = (static_cast<int32_t>(current) - prev) * 32 / time_elapsed;
+  int16_t d_component = -((static_cast<int32_t>(change_rate) * d_) >> 16);
+  
+  prev_ = current;
+  return constrain(static_cast<int32_t>(p_component) + i_component + d_component, INT16_MIN, INT16_MAX);
 }
