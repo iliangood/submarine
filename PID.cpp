15c4221797e86881081407ac8fc716f0069d2809
iliangood <ilia191168@gmail.com>
2026-02-23 21:22:47 +0300

    Работы над PID

--- PID.cpp
+++ PID.cpp
@@ -1,1 +1,18 @@
 #include "PID.h"
+
+int16_t PID::update(int16_t current)
+  {
+    uint16_t time_elapsed = constrain(millis() - last_update_, 1, 50);
+
+    int32_t diff = static_cast<int32_t>(target_) - static_cast<int32_t>(current);
+    
+    int16_t p_component = static_cast<int16_t>(constrain(diff * p_, INT16_MIN, INT16_MAX));
+
+    i_component = constrain(i_component + (diff * i_ * time_elapsed), -i_max_, i_max_);
+    
+    int16_t d_component = constrain(-(static_cast<int32_t>(current) - prev_) * d_ / time_elapsed, INT16_MIN, INT16_MAX);
+    
+    prev_ = current;
+    last_update_ = millis();
+    return constrain(static_cast<int32_t>(p_component) + i_component + d_component, INT16_MIN, INT16_MAX);
+  }
--- PID.h
+++ PID.h
@@ -21,23 +21,14 @@ public:
     target_ = target;
   }
 
-  int16_t update(int16_t current)
+  int16_t update(int16_t current);
+
+  void setcoefficients(float p, float i, float d)
   {
-    uint16_t time_elapsed = constrain(millis() - last_update_, 1, 50);
-
-    int32_t diff = static_cast<int32_t>(target_) - static_cast<int32_t>(current);
-    
-    int16_t p_component = static_cast<int16_t>(constrain(diff * p_, INT16_MIN, INT16_MAX));
-
-    i_component = constrain(i_component + (diff * i_ * time_elapsed), -i_max_, i_max_);
-    
-    int16_t d_component = constrain(-(static_cast<int32_t>(current) - prev_) * d_ / time_elapsed, INT16_MIN, INT16_MAX);
-    
-    prev_ = current;
-    last_update_ = millis();
-    return constrain(static_cast<int32_t>(p_component) + i_component + d_component, INT16_MIN, INT16_MAX);
+    p_ = p;
+    i_ = i;
+    d_ = d;
   }
-
 
 };
 
