072f4a7fd360756ebf47cf7e108cf6830345c89b
iliangood <ilia191168@gmail.com>
2026-03-25 16:04:09 +0300

    Работа над сохранением конфигов

--- config.h
+++ config.h
@@ -1,6 +1,15 @@
 #if !defined (CONFIG_H)
 #define CONFIG_H
+template<>
 class config {
   size_t current_;
+public:
+  template<typename T>
+  T get() {
+    T res = *reinterpeter_cast<*T>(current);
+    current += sizeof(T);
+    return res;
+  }
+
 };
 #endif
